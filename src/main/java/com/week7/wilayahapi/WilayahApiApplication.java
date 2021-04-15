0104d77178fb0e5612203e217a8f72ade17c0031
ulimultia <43207525+ulimultia@users.noreply.github.com>
2021-04-15 14:03:04 +0700

    first commit

---
+++ src/main/java/com/week7/wilayahapi/WilayahApiApplication.java
@@ -0,0 +1,13 @@
+package com.week7.wilayahapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WilayahApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WilayahApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/week7/wilayahapi/WilayahApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week7.wilayahapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WilayahApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
